8316f0310a28b1ad592e546d5d89541e7840c797
becky112358 <8b2@protonmail.com>
2020-06-11 15:05:07 +0300

    Receive an input temperature type.

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,23 @@ fn main() {
     };
 
     println!("You input {}", temperature);
+
+    println!("Please state the temperature type to be converted from.");
+    println!("Options:");
+    println!("C (Celsius)");
+    println!("F (Fahrenheit)");
+    println!("K (Kelvin)");
+
+    let mut temperature_type = String::new();
+
+    io::stdin()
+        .read_line(&mut temperature_type)
+        .expect("Failed to read line.");
+
+    let temperature_type: char = match temperature_type.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return,
+    };
+
+    println!("You input {}", temperature_type);
 }
