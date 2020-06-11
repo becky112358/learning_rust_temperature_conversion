276e3e6271b7063ee63cae0e5f03da19d4cd92f7
becky112358 <8b2@protonmail.com>
2020-06-11 14:24:22 +0300

    Receive an input temperature.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please input the temperature to be converted.");
+
+    let mut temperature = String::new();
+
+    io::stdin()
+        .read_line(&mut temperature)
+        .expect("Failed to read line.");
+
+    let temperature: i32 = match temperature.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return,
+    };
+
+    println!("You input {}", temperature);
 }
